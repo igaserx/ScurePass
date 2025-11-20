d294936f436427f111ea629c6e1e1c2d2d3119df
igaserx <gasseralaa11@gmail.com>
2025-11-20 19:20:47 +0200

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# scure_pass
+My CS50 Final Project - A Secure Password Manager
---
+++ android/app/src/main/kotlin/com/example/scure_pass/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.scure_pass
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
